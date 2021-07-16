4e26becb9c330783c39596f3d830a1817e877b26
neerajdask <iamneerajdas@gmail.com>
2021-07-16 13:25:52 +0530

    Context object with getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,17 +1,33 @@
 import MeetupDetail from '../../components/meetups/MeetupDetail';
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
 	return (
 		<>
 			<MeetupDetail
-				title={'Vue Conf, Berlin'}
-				address={'Berlin, Townhall'}
-				image={
-					'https://upload.wikimedia.org/wikipedia/commons/a/a6/Brandenburger_Tor_abends.jpg'
-				}
-				description={'Germanys best developers'}
+				title={props.meetupData.title}
+				id={props.meetupData.id}
+				address={props.meetupData.address}
+				image={props.meetupData.image}
+				description={props.meetupData.description}
 			/>
 		</>
 	);
 };
+
+export async function getStaticProps(context) {
+	const id = context.params.meetupId;
+
+	return {
+		props: {
+			meetupData: {
+				id: id,
+				title: 'Vue Conf, Berlin',
+				address: 'Berlin, Townhall',
+				image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Brandenburger_Tor_abends.jpg',
+
+				description: 'Germanys best developers',
+			},
+		},
+	};
+}
 
 export default MeetupDetails;
